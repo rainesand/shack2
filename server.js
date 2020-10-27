45f869a3d229e2cfe23b233966d04e8e77f5fc33
Andrew Raines <rainesand26@gmail.com>
2020-10-27 14:55:03 -0400

    working on authentication

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cors());
 app.use(express.json());
 app.use(morgan("app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));"));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
-  // Add routes, both API and view
-  app.use(routes);
   // Connect to the Mongo DB
   mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Shack2");
   // Start the API server
@@ -25,10 +22,5 @@ if (process.env.NODE_ENV === "production") {
     console.log(`:earth_americas:  ==> API Server now listening on PORT ${PORT}!`);
   });
 
-
-
-
-
-
-  // username already taken and some checks code
-  
+   // Add routes, both API and view
+   app.use(routes);
